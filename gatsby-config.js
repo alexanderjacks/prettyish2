f9535a8e29e7b3f7e69b068f0ef737398c781a1c
Alexander Jacks <alexanderthejacks@gmail.com>
2020-10-19 10:02:41 -0700

    🏗  now trunk as main branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-google-sheets',
+      options: {
+        spreadsheetId: '1kMsPdVwJxPHMXMW5lUbtEJqofHYpLFxErg6TQ3MN10w',
+        worksheetTitle: '88names',
+        credentials: require('./path-to-credentials-file.json')
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
